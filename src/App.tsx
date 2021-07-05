30eb58aa7551c58f1c22bb7419beb056a4fa6e91
OH-JOO-YEONG <brb1111@naver.com>
2021-07-05 23:33:18 +0900

    practice: 타입스크립트로 리액트 상태관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Counter from './Counter';
 import Greetings from './Greetings';
+import MyForm from './MyForm';
+import ReducerSample from './ReducerSample';
 
 const App: React.FC = () => {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`);
-  };
-  return <Greetings name="hello" onClick={onClick} />;
+  //   const onClick = (name: string) => {
+  //     console.log(`${name} says hello`);
+  //   };
+  //   return <Greetings name="hello" onClick={onClick} />;
+  // return <Counter />;
+  // const onSubmit = (form: { name: string; description: string }) => {
+  //   console.log(form);
+  // };
+  // return <MyForm onSubmit={onSubmit} />;
+  return <ReducerSample />;
 };
 
 export default App;
