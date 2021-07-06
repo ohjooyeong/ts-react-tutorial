4617180c9cf61d4e5fc1dee535463e2b13629f1d
OH-JOO-YEONG <brb1111@naver.com>
2021-07-06 23:08:34 +0900

    practice: 타입스크립트에서 리덕스 사용하기: 카운터 기능

---
+++ ts-react-redux-tutorial/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import CounterContainer from './containers/CounterContainer';
+import './App.css';
+
+function App() {
+  return <CounterContainer />;
+}
+
+export default App;
