0e09a67cfa3034aa60c182c0bcced008dcb049c1
OH-JOO-YEONG <brb1111@naver.com>
2021-07-09 17:09:21 +0900

    practice: 타입스크립트에서 리덕스 미들웨어 사용하기: redux-thunk함수와 리듀서 리팩토링

--- ts-react-redux-tutorial/src/modules/github/actions.ts
+++ ts-react-redux-tutorial/src/modules/github/actions.ts
@@ -10,4 +10,4 @@ export const getUserProfileAsync = createAsyncAction(
   GET_USER_PROFILE_REQEUST,
   GET_USER_PROFILE_SUCCESS,
   GET_USER_PROFILE_ERROR,
-)<undefined, GithubProfile, AxiosError>();
+)<any, GithubProfile, AxiosError>();
--- ts-react-redux-tutorial/src/modules/github/reducer.ts
+++ ts-react-redux-tutorial/src/modules/github/reducer.ts
@@ -4,41 +4,38 @@ import {
   GET_USER_PROFILE_SUCCESS,
   GET_USER_PROFILE_REQEUST,
   GET_USER_PROFILE_ERROR,
+  getUserProfileAsync,
 } from './actions';
+import { asyncState, createAsyncReducer } from '../../lib/reducerUtils';
 
 const initialState: GithubState = {
-  userProfile: {
-    loading: false,
-    error: null,
-    data: null,
-  },
+  userProfile: asyncState.initial(),
 };
 
-const github = createReducer<GithubState, GithubAction>(initialState, {
-  [GET_USER_PROFILE_REQEUST]: state => ({
-    ...state,
-    userProfile: {
-      loading: true,
-      error: null,
-      data: null,
-    },
-  }),
-  [GET_USER_PROFILE_SUCCESS]: (state, action) => ({
-    ...state,
-    userProfile: {
-      loading: false,
-      error: null,
-      data: action.payload,
-    },
-  }),
-  [GET_USER_PROFILE_ERROR]: (state, action) => ({
-    ...state,
-    userProfile: {
-      loading: false,
-      error: action.payload,
-      data: null,
-    },
-  }),
-});
+// const github = createReducer<GithubState, GithubAction>(initialState, {
+//   [GET_USER_PROFILE_REQEUST]: state => ({
+//     ...state,
+//     userProfile: asyncState.load(),
+//   }),
+//   [GET_USER_PROFILE_SUCCESS]: (state, action) => ({
+//     ...state,
+//     userProfile: asyncState.success(action.payload),
+//   }),
+//   [GET_USER_PROFILE_ERROR]: (state, action) => ({
+//     ...state,
+//     userProfile: asyncState.error(action.payload),
+//   }),
+// });
+
+const github = createReducer<GithubState, GithubAction>(
+  initialState,
+).handleAction(
+  [
+    getUserProfileAsync.request,
+    getUserProfileAsync.success,
+    getUserProfileAsync.failure,
+  ],
+  createAsyncReducer(getUserProfileAsync, 'userProfile'),
+);
 
 export default github;
